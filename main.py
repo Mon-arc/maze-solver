88317e2c4033f75b53d5bbed7b4eb3c432feccb2
Mon-arc <hassanelshikh25@gmail.com>
2025-12-09 12:12:38 -0500

    Final

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from graphics import Window, Line, Cell
+import time
 from maze import Maze
 
 def main():
     win = Window(1280, 720)
 
     maze = Maze(0.025 * 720, 0.025 * 1280, 15, 15, 35, 35, win, seed=0)
-    #maze.solve()
+    maze.solve()
 
     win.wait_for_close()
 
